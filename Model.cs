685d8ec3e041f5196708311d8caf6f996492ccc3
Thomas Castiglione <castiglione@mac.com>
2023-08-06 14:37:46 +0800

    add EF and load posts from db

---
+++ Model.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class EntityContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDb;Initial Catalog=midori");
+    }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public required string Content { get; set; }
+}
